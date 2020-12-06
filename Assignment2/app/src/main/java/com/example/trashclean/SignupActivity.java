555692e3285fae824874621375282dc287887483
huynino96 <huynino96@gmail.com>
2020-12-06 15:56:52 +0700

    Fixing Front-end Design

--- Assignment2/app/src/main/java/com/example/trashclean/SignupActivity.java
+++ Assignment2/app/src/main/java/com/example/trashclean/SignupActivity.java
@@ -73,7 +73,7 @@ public class SignupActivity extends AppCompatActivity {
                         // the auth state listener will be notified and logic to handle the
                         // signed in user can be handled in the listener.
                         if (!task.isSuccessful()) {
-                            Toast.makeText(SignupActivity.this, "Authentication failed." + task.getException(),
+                            Toast.makeText(SignupActivity.this, "Cannot Created account",
                                     Toast.LENGTH_SHORT).show();
                         } else {
                             startActivity(new Intent(SignupActivity.this, MainActivity.class));
